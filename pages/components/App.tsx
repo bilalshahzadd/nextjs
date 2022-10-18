6a076508708c0ef1cc367266fab49d4e05b37ecf
bilalshahzadd <bilalshahzad521@gmail.com>
2022-10-19 04:03:38 +0500

    form update

--- pages/components/App.tsx
+++ pages/components/App.tsx
@@ -15,7 +15,7 @@ export default function App() {
     let neededAmount: number = 167;
 
     // total amount here 
-    let totalAmount: number = 200;
+    let totalAmount: number = 167;
 
     // donor count here
     let donors: number = 42;
@@ -24,7 +24,7 @@ export default function App() {
     collectedAmount = totalAmount - neededAmount;
 
     // function to update the amount 
-    function updateAmount(event: React.ChangeEvent<HTMLFormElement>) {
+    function updateAmount(event: React.FormEvent<HTMLFormElement>) {
 
         // preventing form from reloading the page after submit
         event.preventDefault();
@@ -53,6 +53,14 @@ export default function App() {
             return;
         }
 
+        // updating the donor count
+        if (amount.valueAsNumber == 0) {
+            alert('No amount detected');
+            return;
+        } else {
+            donors += 1;
+        }
+
         // taking the sum of collected and donated amount
         collectedAmount += amount.valueAsNumber;
 
@@ -65,9 +73,6 @@ export default function App() {
         // displaying the needed amount
         elem.innerHTML = neededAmount.toString();
 
-        // updating the donor count
-        donors += 1;
-
         // displaying the donor count
         donorCount.innerHTML = donors.toString();
 
@@ -77,6 +82,7 @@ export default function App() {
             tooltip.style.display = 'none';
             btn.style.display = 'none';
         }
+
     }
 
     return (
@@ -88,7 +94,7 @@ export default function App() {
                 <div className='w-96 mb-3' id='tooltipBox'>
                     <div>
                         <div className="mx-auto container px-4 py-4 bg-[#424242] rounded relative">
-                            <p className=" text-xs font-bold text-white pt-2 pb-2">$<span id='neededAmount'>{neededAmount}</span> still needed for this project</p>
+                            <p className=" text-sm text-white pt-2 pb-2">$<span id='neededAmount' className="font-bold">{neededAmount}</span> still needed for this project</p>
                             <svg className="absolute z-10  bottom-[-10px] " width={16} height={10} viewBox="0 0 16 10" fill="#424242" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M8 10L0 0L16 1.41326e-06L8 10Z" fill="#424242" />
                             </svg>
@@ -102,7 +108,7 @@ export default function App() {
                 </div>
 
                 {/* all the elements are stored in this div */}
-                <div className='detail-container h-[17rem] w-96 border-x flex flex-col border-b'>
+                <div className='detail-container h-[15.5rem] w-96 border-x flex flex-col border-b'>
 
                     {/* text blocks here */}
                     <div className='content flex flex-col ml-5 mt-10 h-40 justify-evenly'>
@@ -120,9 +126,9 @@ export default function App() {
                                 <div className="relative rounded-md shadow-sm">
                                     <form onSubmit={updateAmount} className="flex w-96">
                                         <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-                                            <span className="text-gray-500 sm:text-sm">$</span>
+                                            <span className="text-gray-500 sm:text-sm font-bold">$</span>
                                         </div>
-                                        <input type="number" name="amount" id="amount" className="rounded border-gray-300 pl-7 pr-12 focus:border- focus:ring-indigo-500 sm:text-sm h-10 w-24 border apperance" required />
+                                        <input type="number" name="amount" id="amount" className="rounded border-gray-300 pl-7 focus:border- focus:ring-indigo-500 sm:text-sm h-10 w-24 border apperance font-bold" required />
                                         <button className='btn-primary border h-10 text-center mx-2 w-24 bg-[#00be1c] text-white rounded' id='giveButton'>Give Now</button>
                                     </form>
                                 </div>
@@ -137,9 +143,9 @@ export default function App() {
                 </div>
 
                 {/* buttons */}
-                <div className='flex mt-5'>
-                    <button className='pl-10 pr-10 pt-2 pb-2 bg-[#fafafa] text-[#777777] mx-1 border rounded'>Save for later</button>
-                    <button className='pl-10 pr-10 pt-2 pb-2 bg-[#fafafa] text-[#777777] mx-1 border rounded'>Tell your friends</button>
+                <div className='flex mt-4'>
+                    <button className='pl-9 pr-9 pt-2 pb-2 bg-[#fafafa] text-[#777777] mx-1 border rounded font-bold'>Save for later</button>
+                    <button className='pl-9 pr-9 pt-2 pb-2 bg-[#fafafa] text-[#777777] mx-1 border rounded font-bold'>Tell your friends</button>
                 </div>
 
             </div>
--- pages/components/Modal.tsx
+++ pages/components/Modal.tsx
@@ -2,13 +2,13 @@ export default function Modal() {
 
     interface data {
         heading: string,
-        paragraph: string
+        body: string
     }
 
 
     const modalInfo: data = {
         heading: "Next App",
-        paragraph: "Some random information about the app, Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum neque, dolor dignissimos possimus illum obcaecati architecto delectus. Possimus rem, sapiente aut quisquam mollitia pariatur, dolorum dolorem totam suscipit minus iusto."
+        body: "Some random information about the app, Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum neque, dolor dignissimos possimus illum obcaecati architecto delectus. Possimus rem, sapiente aut quisquam mollitia pariatur, dolorum dolorem totam suscipit minus iusto."
     }
 
     // function to toggle modal
@@ -29,7 +29,7 @@ export default function Modal() {
                     <div className='inline-block align-center bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full' role='dialog' aria-modal='true' aria-labelledby='modal-headline'>
                         <div className='bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4'>
                             <h1 className="font-bold text-lg mb-2">{modalInfo.heading}</h1>
-                            <p>{modalInfo.paragraph}</p>
+                            <p>{modalInfo.body}</p>
                         </div>
                         <div className='bg-gray-200 px-4 py-3 text-right'>
                             <button type='button' className='py-2 px-4 bg-gray-500 text-white rounded hover:bg-gray-700 mr-2' onClick={toggleModal}>
